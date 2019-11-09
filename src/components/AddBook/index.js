466b0ad1a1c15d06e7bc8dff872f17feb2ea440f
Conrad Wright <easp.conrad@gmail.com>
2019-11-09 14:39:43 -0700

    fixup! Add AddBook Component

--- src/components/AddBook/index.js
+++ src/components/AddBook/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
 
 import { PageHeader } from '../../componentStash/GenericElements'
 export default class AddBook extends Component {
@@ -10,7 +11,55 @@ export default class AddBook extends Component {
     return (
       <div>
         <PageHeader header={this.state.pageTitle} />
-        <h3>Adding a book form</h3>
+        <Form>
+          <FormGroup>
+            <Label for='author-name'>Author</Label>
+            <Input
+              type='text'
+              name='author-name'
+              placeholder="Enter the Author's name"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for='book-summary'>Book Summary</Label>
+            <Input
+              type='text'
+              name='book-summary'
+              placeholder='Enter a short summary'
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for='cover-url'>Paste in the url of a cover image</Label>
+            <Input type='url' name='url' placeholder='https://example.com' />
+          </FormGroup>
+          <FormGroup tag='fieldset'>
+            <legend>Radio Buttons</legend>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio1' /> Option one is this and
+                that—be sure to include why it's great
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio1' /> Option two can be something
+                else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check disabled>
+              <Label check>
+                <Input type='radio' name='radio1' disabled /> Option three is
+                disabled
+              </Label>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type='checkbox' /> Check me out
+            </Label>
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       </div>
     )
   }
